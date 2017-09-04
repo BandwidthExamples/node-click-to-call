60e081fa64c148c3acf6bd5ff0cdce4dffc502d8
avbel <belchikovs@gmail.com>
2017-09-04 16:10:19 +0300

    Started tests implementations

---
+++ test/index.test.js
@@ -0,0 +1,45 @@
+const test = require('ava');
+const fs = require('mz/fs');
+const td = require('testdouble');
+const supertest = require('supertest');
+const mongoose = require('mongoose');
+const {middlewares} = require('@bandwidth/node-bandwidth-extra');
+
+process.env.BANDWIDTH_USER_ID = 'userId';
+process.env.BANDWIDTH_API_TOKEN = 'token';
+process.env.BANDWIDTH_API_SECRET = 'secret';
+
+td.replace(middlewares, 'koa');
+td.replace(mongoose, 'connect');
+
+const main = require('../lib/index');
+
+test.before(async () => {
+	if (!(await fs.exists('./frontend/build/index.html'))) {
+		try {
+			await fs.mkdir('./frontend/build/');
+		} catch (err) {
+		}
+		await fs.writeFile('./frontend/build/index.html', ' ');
+	}
+});
+
+test('GET / should render index page', async t => {
+	td.when(middlewares.koa(td.matchers.anything())).thenReturn((ctx, next) => next());
+	const app = await main();
+	await supertest(app.callback()).get('/')
+		.expect(200)
+		.expect('Content-Type', /html/);
+	t.pass();
+});
+
+test('should handle calls events', async t => {
+	let callCallback = null;
+	td.when(middlewares.koa(td.matchers.anything())).thenDo(opts => {
+		callCallback = opts.callCallback;
+		return (ctx, next) => next();
+	});
+	const app = await main();
+	await supertest(app.callback()).get('/').expect(200);
+	callCallback()
+});
